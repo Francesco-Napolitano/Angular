dfcb51f4b68d0efe273834b8b96eaf202f654955
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-04-11 16:20:33 +0200

    event-listeners

--- first-angular-app/src/app/home/home.component.ts
+++ first-angular-app/src/app/home/home.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { GreetingComponent } from '../components/greeting/greeting.component';
+import { CounterComponent } from '../components/counter/counter.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [GreetingComponent, CounterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  homeMessage = signal('Hello, world!')
+  
+  keyUpHandler(event: KeyboardEvent){
+    console.log(`user typed something in the input: ${event.key} #FREEBOSSETTI`)
+  }
 }
