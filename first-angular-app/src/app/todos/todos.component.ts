43f327bdd3d02c33dde787e184015fb8d73579aa
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-04-16 12:26:10 +0200

    angular pipes

--- first-angular-app/src/app/todos/todos.component.ts
+++ first-angular-app/src/app/todos/todos.component.ts
@@ -3,15 +3,18 @@ import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
 import { TodoItemComponent } from '../components/todo-item/todo-item.component';
+import { FormsModule } from '@angular/forms';
+import { FilterTodosPipe } from '../pipes/filter-todos.pipe';
 
 @Component({
   selector: 'app-todos',
-  imports: [TodoItemComponent],
+  imports: [TodoItemComponent, FormsModule, FilterTodosPipe],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
 export class TodosComponent implements OnInit{
   todoService = inject(TodosService)
+  searchTerm = signal('')
 
   todoItems = signal<Array<Todo>>([])
 
